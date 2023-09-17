82f4c6e35b05e43f08266cfa3dd5fcbbfc2f79a5
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-17 17:12:24 +0530

    models created

--- product/models.py
+++ product/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 # Create your models here.
+
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=100)
+    parent = TreeForeignKey(
+        "self", on_delete=models.PROTECT, null=True, blank=True
+        )
+
+    def ___str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    def ___str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    is_digital = models.BooleanField(default=False)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = TreeForeignKey(
+        'Category', null=True, on_delete=models.SET_NULL, blank=True
+    )
+
+    def ___str__(self):
+        return self.name
--- product/views.py
+++ product/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
+from .models import Category, Product, Brand
+from .serializers import CategorySerializer, ProductSerializer, BrandSerializer
 # Create your views here.
+
+
+class CategoryViewSet(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing categories.
+    """
+    queryset = Category.objects.all()
+
+    @extend_schema(responses=CategorySerializer)
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+# class BrandViewSet(viewsets.ViewSet):
+#     """
+#     A simple Viewset for viewing brands.
+#     """
+#     queryset = Brand.objects.all()
+
+#     @extend_schema(responses=BrandSerializer)
+#     def list(self, request):
+#         serializer = BrandSerializer(self.queryset, many=True)
+#         return Response(serializer.data)
+
+
+# class ProductViewSet(viewsets.ViewSet):
+#     """
+#     A simple Viewset for viewing Products.
+#     """
+#     queryset = Product.objects.all()
+
+#     @extend_schema(ProductSerializer)
+#     def list(self, request):
+#         serializer = ProductSerializer(self.queryset, many=True)
+#         return Response(serializer.data)
